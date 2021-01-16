388d623718004327e6566091863d162a5a839fba
Shinkei <ing.jorgeivanramirezh@gmail.com>
2021-01-16 01:49:38 +0100

    Add some styles, comments and error handling
    add some styles to make the app looks better, also comments on the
    functions that are involved in the whole process and error handling for
    possible error on the svg files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Poligono
+
+This project aims to calculate the center of a polygon taking an svg as input, the svg should have a polygon tag for this to work.
+Poligono uses react to let the user upload the svg file and render the svg as an image, also renders a circle where the center of the polgon is clculated
+
+## Technologies
+
+- react
+
+## About the calculation
+
+To calculate the center of the polygon, the [formula for non-self-intersecting closed polygon](https://en.wikipedia.org/wiki/Centroid#Of_a_polygon) is used.
+
+## Author
+
+Jorge Ramirez
--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import "./App.css";
 
 function App() {
   const [svgFile, setSvgFile] = useState(null);
+  const [showError, setShowError] = useState(false);
   const fileReader = new FileReader();
-  const regexPolygonPoints = /(?<=<polygon.*points=)(['"])[\d. \n]+\1/gi;
+  // const regexPolygonPoints = /(?<=<polygon.*points=)(['"])[\d. \n]+\1/gi;
+  const regexPolygonPoints = /(?<=<polygon.*points=)(['"])[\d., \n-]+\1/gi;
 
+  /**
+   * Clean the list of points from strange characters and start building a list that contains object {x,y}
+   * referencing the points of the polygon
+   * @param {String} pointsString - list of values separated by comma and/or space
+   */
   const getPointsList = (pointsString) => {
     let str = pointsString.replace(/[^\d. \n]/g, "");
     const listNumbers = str.split(/[, \n]/);
@@ -20,6 +27,13 @@ function App() {
     return pointsList;
   };
 
+  /**
+   * Using the formula for calculate the center of a non-self-intersecting closed polygon,
+   * https://en.wikipedia.org/wiki/Centroid#Of_a_polygon
+   * go trough all the points of the poligon and apply the formula creating triagles between
+   * the current point and the first point of the polygon
+   * @param {Array[{x:number,y:number}]} points - list of points
+   */
   const calculateCenter = (points) => {
     let firstPoint = points[0];
     let lastPoint = points[points.length - 1];
@@ -46,36 +60,61 @@ function App() {
     };
   };
 
+  /**
+   * Renders the svg file but put a circle that is rendered in the center of the polygon
+   * @param {Object{x:number, y:number}} center - points x and y
+   * @param {String} content - the content og the svg file to be rendered
+   */
   const insertCenter = (center, content) => {
     const [firstPart, ...rest] = content.split("</svg>");
     const result = `
       ${firstPart}
-      <circle cx="${center.x}" cy="${center.y}" r="10" stroke="black" stroke-width="3" fill="red" />
+      <circle cx="${center.x}" cy="${center.y}" r="10" stroke="black" stroke-width="3" fill="#fd5a3c" />
       ${rest}
       </svg
     `;
     return result;
   };
 
+  /**
+   * Get the file from the input reader and take the points property using regex,
+   * convert the list of values and convert it into a list of point ojects to calculate the
+   * center of the svg
+   */
   const readFile = () => {
     const content = fileReader.result;
-    const [pointsString] = content.match(regexPolygonPoints);
-    const pointsList = getPointsList(pointsString);
+    const pointsString = content.match(regexPolygonPoints);
+    console.log(content);
+    if (!pointsString) {
+      setShowError(true);
+      return;
+    }
+    const pointsList = getPointsList(pointsString[0]);
     const centerPoint = calculateCenter(pointsList);
     const svgResult = insertCenter(centerPoint, content);
     setSvgFile(svgResult);
   };
 
-  fileReader.onloadend = readFile;
-
+  /**
+   * Get the file that is uploaded for the file input and sent to the FileReader
+   * @param {Event} event - click event for the file input
+   */
   const uploadFile = (event) => {
+    setShowError(false);
+    setSvgFile(null);
     fileReader.readAsText(event.target.files[0]);
   };
 
+  // When the file is loaded by the fileReader, then the readFile function is called
+  fileReader.onloadend = readFile;
+
   return (
     <div className="App">
-      <input type="file" accept=".svg" onChange={uploadFile} />
-      {/* <img src={image} alt="polygon"/ */}
+      <label className="uploadButton">
+        Upload Polygon
+        <input type="file" accept=".svg" onChange={uploadFile} />
+      </label>
+      {showError && <p className="errorText">Error uploading file</p>}
       {svgFile && <div dangerouslySetInnerHTML={{ __html: svgFile }} />}
     </div>
   );
